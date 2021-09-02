ee02129b1431d682e703eca714fe4b3843b71b56
BubakVOE <ludwigtom@seznam.cz>
2021-09-02 23:05:47 +0200

    test

---
+++ resources/views/components/home-content.blade.php
@@ -0,0 +1,151 @@
+<div class="relative xl:h-full xl:w-10/12 xl:m-auto xl:flex">
+
+    <div class="mx-auto w-full  px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 h-96 flex justify-center">
+        <div class="sm:text-center lg:text-left flex flex-col justify-center items-center">
+            <div class=" xl:flex xl:flex-col xl:justify-center xl:items-center">
+                <h1 class="text-4xl font-bold text-gray-900 sm:text-5xl md:text-6xl">Nástříky dutin</h1>
+                <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">osobní auta, SUV, Off-road </h1>
+            </div>
+
+            <div>
+                <h1 class="xl:mt-3 text-black sm:mt-16 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                    Příjemná, malá společnost, která děla vše poctivě !
+                </h1>
+            </div>
+
+            <div class="mt-5 sm:flex sm:justify-center lg:justify-start">
+                <div class="rounded-md shadow">
+                    <a href="#" class="font-montserrat w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                    Rezervovat
+                    </a>
+                </div>
+                <div class="mt-3 sm:mt-0 sm:ml-3 ">
+                    <a href="#" class="font-montserrat w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                    Galerie
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="mx-auto w-full px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 h-96 ">
+        {{-- <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt=""> --}}
+        <div class="slideshow-container text-center flex flex-col items-center justify-evenly w-full h-full ">
+
+            <div class="w-full flex justify-center">
+
+                <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
+                    <div class="w-10/12 m-auto ">
+                        <img class="" src="{{ asset('img/test/map12.png') }}" >
+                    </div>
+                    <div>
+                        <h1 class="text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda octavia 2013 </h1>
+                    </div>
+                </div>
+                
+                <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
+                    <div class="w-10/12 m-auto ">
+                        <img class="" src="{{ asset('img/test/map12.png') }}" >
+                    </div>
+
+                    <div>
+                        <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda SuperB 2013 </h1>
+                    </div>
+                </div>
+                
+                <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
+                    <div class="w-10/12 m-auto ">
+                        <img class="" src="{{ asset('img/test/map12.png') }}" >
+                    </div>
+
+                    <div>
+                        <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda KodiaQ 2013 </h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-5/12 flex justify-evenly">
+                <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
+                <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
+                <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class=" w-10/12 m-auto mt-1/12 pb-10 ">
+
+    <div class="w-full flex justify-evenly items-center text-center">
+        <div class="w-full flex justify-center">
+            <div class="bg-gray-500 rounded-md w-60 h-60 flex justify-center items-center">
+
+                <a href="" class="relative block w-full transition duration-300 pb-full rounded-xl bg-primary-200 text-primary-800 hover:bg-primary-800 hover:text-white ">
+
+                    <span class="flex flex-col justify-center items-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
+                            </svg>
+                        </span>
+
+                        <span>
+                            Nástříky
+                        </span>
+                    </span>
+                </a>
+            </div>
+        </div>
+
+        <div class="w-full flex justify-center">
+            <div class="bg-gray-500 rounded-md w-60 h-60 flex justify-center items-center">
+                <a href="" class="relative block w-full transition duration-300 pb-full rounded-xl bg-primary-200 text-primary-800 hover:bg-primary-800 hover:text-white">
+
+                    <span class="flex flex-col justify-center items-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                            </svg>
+                        </span>
+    
+                        <span>
+                            Přezutí gum
+                        </span>
+                    </span>
+                </a>
+            </div>
+        </div>
+
+        <div class="w-full flex justify-center">
+            <div class="bg-gray-500 rounded-md w-60 h-60 flex justify-center items-center">
+
+                <a href="" class="relative block w-full transition duration-300 pb-full rounded-xl bg-primary-200 text-primary-800 hover:bg-primary-800 hover:text-white">
+
+                    <span class="flex flex-col justify-center items-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                            </svg>
+                        </span>
+
+                        <span>
+                            Vyčistění auta
+                        </span>
+                    </span>
+
+                </a>
+            </div>
+        </div>
+    </div>
+
+</section>   
+    
+
+
+
+
+  {{-- 
+    
+    object-cover sm:h-72 md:h-96 lg:w-full lg:h-full xl:h-20 xl:w-20
+    object-cover sm:h-72 md:h-96 lg:w-full lg:h-full xl:h-20 xl:w-20
+    
+    --}}
---
+++ tailwind.config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+      height: {
+        '1/12':  '8.333333%',
+        '2/12':	'16.666667%',
+        '3/12':	'25%',
+        '4/12':	'33.333333%',
+        '5/12':	'41.666667%',
+        '6/12':	'50%',
+        '7/12':	'58.333333%',
+        '8/12':  '66.666667%',
+        '9/12':  '75%',
+        '10/12':	'83.333333%',
+        '11/12':	'91.666667%',
+      },
+
+      margin: {
+        '1/12':  '8.333333%',
+        '2/12':	'16.666667%',
+        '3/12':	'25%',
+        '4/12':	'33.333333%',
+        '5/12':	'41.666667%',
+        '6/12':	'50%',
+        '7/12':	'58.333333%',
+        '8/12':  '66.666667%',
+        '9/12':  '75%',
+        '10/12':	'83.333333%',
+        '11/12':	'91.666667%',
+      },
+
+      colors: {
+        red: {
+          'custom' : '#e30022',
+        },
+        gray: {
+          'dark' : '#0F0F0F',
+          'medium' : '#1C1D21',
+          'light' : '#252425',
+        },
+
+        black: {
+          'custom' :'#000000',
+        },
+
+
+        blue: {
+          'light': '#00d9ff',
+          'dark' : '#0011ff',
+          'glass': '#577AB4',
+          'dog': '#577AB4',
+        },
+      },
+
+      fontFamily: {
+        montserrat: 'Montserrat, sans-serif',
+        poppins: 'Poppins, sans-serif',
+        apple: '-apple-system, BlinkMacSystemFont Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif',
+        inter: 'Inter, sans-serif',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
