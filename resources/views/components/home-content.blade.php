77cb389c52cae160b99e8a0870a600dae07e8875
BubakVOE <ludwigtom@seznam.cz>
2021-09-03 20:19:13 +0200

    save

--- resources/views/components/home-content.blade.php
+++ resources/views/components/home-content.blade.php
@@ -1,79 +1,83 @@
-<div class="relative xl:h-full xl:w-10/12 xl:m-auto xl:flex">
+<div class="  w-full my-16 bg-gray-300 ">
+    <div class="relative xl:w-10/12 m-auto ">
+        <div class="xl:flex xl:items-center justify-center ">
 
-    <div class="mx-auto w-full  px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 h-96 flex justify-center">
-        <div class="sm:text-center lg:text-left flex flex-col justify-center items-center">
-            <div class=" xl:flex xl:flex-col xl:justify-center xl:items-center">
-                <h1 class="text-4xl font-bold text-gray-900 sm:text-5xl md:text-6xl">Nástříky dutin</h1>
-                <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">osobní auta, SUV, Off-road </h1>
-            </div>
-
-            <div>
-                <h1 class="xl:mt-3 text-black sm:mt-16 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                    Příjemná, malá společnost, která děla vše poctivě !
-                </h1>
-            </div>
-
-            <div class="mt-5 sm:flex sm:justify-center lg:justify-start">
-                <div class="rounded-md shadow">
-                    <a href="#" class="font-montserrat w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                    Rezervovat
-                    </a>
-                </div>
-                <div class="mt-3 sm:mt-0 sm:ml-3 ">
-                    <a href="#" class="font-montserrat w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                    Galerie
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="mx-auto w-full px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 h-96 ">
-        {{-- <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt=""> --}}
-        <div class="slideshow-container text-center flex flex-col items-center justify-evenly w-full h-full ">
-
-            <div class="w-full flex justify-center">
-
-                <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
-                    <div class="w-10/12 m-auto ">
-                        <img class="" src="{{ asset('img/test/map12.png') }}" >
+            <div class="mx-auto w-full  px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-0 h-96 flex items-center justify-center ">
+                <div class="sm:text-center lg:text-left flex flex-col justify-center items-center">
+                    <div class=" xl:flex xl:flex-col xl:justify-center xl:items-center">
+                        <h1 class="text-4xl font-bold text-gray-900 sm:text-5xl md:text-6xl">Nástříky dutin</h1>
+                        <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">osobní auta, SUV, Off-road </h1>
                     </div>
+        
                     <div>
-                        <h1 class="text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda octavia 2013 </h1>
+                        <h1 class="xl:mt-3 text-black sm:mt-16 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                            Příjemná, malá společnost, která děla vše poctivě !
+                        </h1>
                     </div>
-                </div>
-                
-                <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
-                    <div class="w-10/12 m-auto ">
-                        <img class="" src="{{ asset('img/test/map12.png') }}" >
-                    </div>
-
-                    <div>
-                        <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda SuperB 2013 </h1>
-                    </div>
-                </div>
-                
-                <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
-                    <div class="w-10/12 m-auto ">
-                        <img class="" src="{{ asset('img/test/map12.png') }}" >
-                    </div>
-
-                    <div>
-                        <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda KodiaQ 2013 </h1>
+        
+                    <div class="mt-5 sm:flex sm:justify-center lg:justify-start">
+                        <div class="rounded-md shadow">
+                            <a href="#" class="font-montserrat w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                            Rezervovat
+                            </a>
+                        </div>
+                        <div class="mt-3 sm:mt-0 sm:ml-3 ">
+                            <a href="#" class="font-montserrat w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                            Galerie
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="w-5/12 flex justify-evenly">
-                <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
-                <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
-                <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
+        
+            <div class="mx-auto w-full px-4 sm:mt-5 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-0 h-96 flex items-center justify-cente ">
+                {{-- <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt=""> --}}
+                <div class="slideshow-container text-center flex flex-col items-center justify-evenly w-full h-full ">
+        
+                    <div class="w-full flex justify-center">
+        
+                        <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
+                            <div class="w-10/12 m-auto ">
+                                <img class="" src="{{ asset('img/test/map12.png') }}" >
+                            </div>
+                            <div>
+                                <h1 class="text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda octavia 2013 </h1>
+                            </div>
+                        </div>
+                        
+                        <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
+                            <div class="w-10/12 m-auto ">
+                                <img class="" src="{{ asset('img/test/map12.png') }}" >
+                            </div>
+        
+                            <div>
+                                <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda SuperB 2013 </h1>
+                            </div>
+                        </div>
+                        
+                        <div class="mySlides fade w-5/12 flex justify-center items-center text-center   ">
+                            <div class="w-10/12 m-auto ">
+                                <img class="" src="{{ asset('img/test/map12.png') }}" >
+                            </div>
+        
+                            <div>
+                                <h1 class=" text-gray-500 sm:text-lg md:text-xl md:mt-2 xl:mt-3">Škoda KodiaQ 2013 </h1>
+                            </div>
+                        </div>
+                    </div>
+        
+                    <div class="w-5/12 flex justify-evenly">
+                        <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
+                        <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
+                        <span class="dot bg-red-500 px-5 py-1 rounded-xl"></span> 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<section class=" w-10/12 m-auto mt-1/12 pb-10 ">
+<section class=" w-10/12 m-auto pb-10 ">
 
     <div class="w-full flex justify-evenly items-center text-center">
         <div class="w-full flex justify-center">
